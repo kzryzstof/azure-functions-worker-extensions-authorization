4780fef525182a88fe28e076bd1e87b27a359a8f
Christophe Commeyne <christophecommeyne@nosuchcompany.ca>
2023-01-19 14:50:44 -0500

    .

--- NoSuchCompany.Azure.Functions.Worker/src/Extensions.Bindings/ClaimsPrincipalInputAttribute.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
-
-namespace NoSuchCompany.Azure.Functions.Worker.Extensions.Bindings;
-
-[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
-public sealed class ClaimsPrincipalInputAttribute : InputBindingAttribute
-{
-}
--- NoSuchCompany.Azure.Functions.Worker/src/Extensions.Bindings/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
-
-[assembly: ExtensionInformation("NoSuchCompany.Azure.Functions.Worker.Extensions.Bindings", "1.0.0")]
